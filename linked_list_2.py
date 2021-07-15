41404b0beea9dcd238e1219db7839f91ef69d043
QCfarmer <61468622+QCfarmer@users.noreply.github.com>
2021-07-15 11:31:16 +0800

    Update linked_list_2.py

--- linked_list_2.py
+++ linked_list_2.py
@@ -1,8 +1,17 @@
+'''
+改自原书代码 python/06_linkedlist/singly_linked_list.py
+https://github.com/wangzheng0822/algo/blob/b2c1228ff915287ad7ebeae4355fa26854ea1557/python/06_linkedlist/singly_linked_list.py
+为理解给原书代码添加了注释和部分打印消息
+
+主要是考虑到想要添加重复的节点对原书代码进行修改
+添加和删除的时候操作重点是节点的值
+'''
+
 class Node:
     def __init__(self, data, next_node=None):
         self.data = data
         self._next = next_node
-    def __repr__(self):
+    def __repr__(self): #为实现打印链表，重写了节点的打印方法
         if self._next == None:
             return '{}'.format(self.data)
         else:
@@ -40,13 +49,14 @@ class SinglyLinkedList:
             # new_node._next = self._head
             # self._head = new_node
 
-            # 通过复制插入名称重复的节点
+            # 通过复制可插入名称重复的节点
             copy_node = Node(new_node.data)
             copy_node._next = self._head
             self._head = copy_node
         else:
             print('empty node')
-
+            
+    # 既然有从头插入，理应也有尾插
     def insert_value_to_tail(self,value:int):
         new_node = Node(value)
         self.insert_node_to_tail(new_node)
@@ -58,10 +68,11 @@ class SinglyLinkedList:
             p = self._head
             while p._next != None:
                 p = p._next
+            
             # 原书代码
             # p._next = new_node
 
-            #通过复制插入名称重复的节点
+            #通过复制可插入名称重复的节点
             copy_node = Node(new_node.data)
             p._next = copy_node
 
@@ -74,11 +85,12 @@ class SinglyLinkedList:
         if not node or not new_node:
             return
         else:
+            
             # 原书代码
             # new_node._next = node._next
             # node._next = new_node
 
-            #通过复制插入名称重复的节点
+            #通过复制可插入名称重复的节点
             copy_node = Node(new_node.data)
             copy_node._next = node._next
             node._next = copy_node
@@ -89,25 +101,26 @@ class SinglyLinkedList:
 
     def insert_node_before_node(self, node: Node, new_node: Node):
         # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        if not self._head or not node or not new_node:#空链表
+        if not self._head or not node or not new_node: #空链表
             print('empty linked list or')
             return
-        elif self._head == node:#该节点为头节点
+        elif self._head == node: #该节点为头节点
             self.insert_node_to_head(new_node)
             return
         else:
             current = self._head
-            while current._next and current._next != node:#在尾节点之前寻找该节点
+            while current._next and current._next != node: #在尾节点之前寻找该节点
                 current = current._next
-            if not current._next:  #没有找到该节点
+            if not current._next: #没有找到该节点
                 print('no such node')
                 return
             else:
+                
                 # 原书代码
                 # new_node._next = node
                 # current._next = new_node
 
-                # 通过复制插入名称重复的节点
+                # 通过复制可插入名称重复的节点
                 copy_node = Node(new_node.data)
                 copy_node._next = node
                 current._next = copy_node
@@ -118,7 +131,7 @@ class SinglyLinkedList:
     #     if not self._head or not node:#空链表
     #         print('empty linked list')
     #         return
-    #     elif node._next:#要删除的节点不是尾节点
+    #     elif node._next: #要删除的节点不是尾节点
     #         #将要删除的节点直接修改为下一节点，即改变了当前位置的数据
     #         node.data = node._next.data
     #         node._next = node._next._next
@@ -128,33 +141,41 @@ class SinglyLinkedList:
     #         # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     #         while current and current._next != node:
     #             current = current._next
-    #         if not current:#遍历完整个链表没有找到该节点current=None
+    #         if not current: #遍历完整个链表没有找到该节点current=None
     #             print('no such node')
     #             return
     #         else:
     #             current._next = node._next
 
+    # 重写一个删除节点的方法，参照插入值的方法，用删除值实现
     def delete_by_node(self, node: Node):
         value = node.data
         self.delete_by_value(value)
 
-    def delete_by_value(self, value: int):#删除所有为该值的节点，不会显示链表中是否存在该值
-        if not self._head or not value:#空链表或删除的值为空
+    def delete_by_value(self, value: int): #删除所有为该值的节点，不会显示链表中是否存在该值
+        if not self._head or not value: #空链表或删除的值为空
             print('empty linked list or value')
             return
         else:
-            fake_head = Node(value + 1)
+            # 原书代码
+            # 假定节点的值都为int
+            # fake_head = Node(value + 1)
+            
+            fake_head = Node('fake')
+            
             fake_head._next = self._head
             prev, current = fake_head, self._head
-            while current:#当前值不为尾节点
+            while current: #当前值不为尾节点
                 if current.data != value:
                     prev._next = current
                     prev = prev._next
                 current = current._next
             if prev._next:
                 prev._next = None
-            self._head = fake_head._next#该值正好是头节点
+            self._head = fake_head._next #该值正好是头节点
 
+    # 原书代码
+    # 通过复制链表节点的值实现打印
     # def __repr__(self) -> str:
     #     nums = []
     #     current = self._head
@@ -163,6 +184,7 @@ class SinglyLinkedList:
     #         current = current._next
     #     return "->".join(str(num) for num in nums)
     
+    # 改写的方法，通过打印节点的信息打印整个链表
     def __repr__(self):
         if self._head == None:
             return 'empty linked list'
@@ -175,7 +197,9 @@ class SinglyLinkedList:
         while node:
             yield node.data
             node = node._next
-
+            
+    # 原书代码
+    # 感觉用不到
     # def print_all(self):
     #     current = self._head
     #     if current:
@@ -209,6 +233,7 @@ if __name__ == "__main__":
     node66 = Node(66)
     print(node66)
     node67 = l.find_by_index(0)
+    # 测试是否能插入重复的节点
     l.insert_node_before_node(node67,node66)
     l.insert_node_before_node(node67,node66)
     l.insert_node_to_head(node66)
